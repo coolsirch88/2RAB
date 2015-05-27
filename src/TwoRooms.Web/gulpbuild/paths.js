49a3d4d3fa3c1e6f7968bd149c7cb3da6802df0e
cwalling <coolsirch88@gmail.com>
2015-05-27 00:22:08 -0400

    Trying to fix the gitignore

---
+++ src/TwoRooms.Web/gulpbuild/paths.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+var appRoot = 'wwwroot/src/';
+var outputRoot = 'wwwroot/dist/';
+
+module.exports = {
+  root: appRoot,
+  source: appRoot + '**/*.js',
+  html: appRoot + '**/*.html',
+  style: 'styles/**/*.css',
+  output: outputRoot,
+  sourceMapRelativePath: '../' + appRoot,
+  doc:'./doc',
+  e2eSpecsSrc: 'test/e2e/src/*.js',
+  e2eSpecsDist: 'test/e2e/dist/'
+};
